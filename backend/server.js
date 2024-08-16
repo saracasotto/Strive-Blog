2ef0b5c9b3a06c911bdfecb9fba6fc94ed2cc494
saracasotto <saracasotto.dev@gmail.com>
2024-08-16 12:32:47 +0100

    server

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 import express from "express";
 
 const server = express();
-const port = 3001
+const port = 3000
+
 
 server.get('/authors', (request, response) => {
     response.send('ciao belli')
@@ -15,10 +16,11 @@ server.get('/authors/:id', (request, response) =>{
 
 server.post('authors', (request, response) => {
     response.send(request.body)
+    //ricevo un json
 })
 
 server.listen(port, () => {
     console.log(`Server is running at port ${port}`)
-}) //server in ascolto alla porta 3001
+}) //server in ascolto alla porta 300
 
 //IMPORTANTE! RIAVVIA SERVER A OGNI MODIFICA
